e4045da75b9851152bbbb30d7142ccf2dff9e3cf
miscmike <miscmikesyd@gmail.com>
2023-04-02 21:31:04 +1000

    add flames and such

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject car;
+    public Rigidbody carRB;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Car")
+        {
+            car.transform.position = new Vector3(-38.2299995f, 0.388999999f, 0);
+            carRB = car.GetComponent<Rigidbody>();
+
+            carRB.velocity = Vector3.zero;
+            carRB.rotation = Quaternion.identity;
+        }
+        
+
+    }
+}
